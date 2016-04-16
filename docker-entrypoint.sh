654b19b1684e6d76b1b32c678cca55aa0455e1b7
HKLCF <hklcf@users.noreply.github.com>
2016-04-17 03:13:50 +0800

    disable log

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # ocdownloader requirements
-su - aria2 -c '/usr/bin/aria2c --enable-rpc --rpc-allow-origin-all -c -D --log=/dev/stdout --check-certificate=false &'
+su - aria2 -c '/usr/bin/aria2c --enable-rpc --rpc-allow-origin-all -c -D --check-certificate=false &'
 
 # makes aria2 and www-data works together
 umask 0007
